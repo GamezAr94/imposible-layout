dbf02465b9799483ea1bf67205fcffb188ab11b9
arturo <arturogamez51@gmail.com>
2024-02-20 23:16:58 -0800

    starting to add more container elements

--- impossible_layout/src/components/banner.js
+++ impossible_layout/src/components/banner.js
@@ -8,8 +8,31 @@ function Banner() {
             <div className={styles.center_banner}>
                 <div className={styles.portrait_img}>
                     Hola
+                    <div className={styles.bottom_container}>
+                        <div className={styles.date_container}>2</div>
+                        <div className={styles.rest_content_dnd}>D&D</div>
+                        <svg className={styles.svg_bottom_dnd}>
+                            <defs>
+                                <filter id="goo">
+                                    <feGaussianBlur
+                                        in="SourceGraphic"
+                                        stdDeviation="10"></feGaussianBlur>
+                                    <feColorMatrix
+                                        in="name"
+                                        mode="matrix"
+                                        values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -15"></feColorMatrix>
+                                    <feBlend in="SourceGraphic"></feBlend>
+                                </filter>
+                            </defs>
+                        </svg>
+                    </div>
                     <div className={styles.container_btn_lft}>
-                        container bottom left
+                        <div className={styles.diagonal_left}></div>
+                        <div className={styles.container_box}>
+                            <div className={styles.main_container_left_box}>
+                                hola
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.container_top_center}>
                         container top center
